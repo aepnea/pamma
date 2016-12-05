ea5e078dcf6b05cd50f3836fd91c74ec115e15a4
Cristian Vásquez Parga <cvasquez@zaetachile.cl>
2016-12-05 02:57:15 -0300

    cambio culiao

--- app/models/benefit_requested.rb
+++ app/models/benefit_requested.rb
@@ -4,4 +4,7 @@ class BenefitRequested < ActiveRecord::Base
   belongs_to :asociative_user
 
   belongs_to :postulation
+#  has_one :milestone
+#  accepts_nested_attributes_for :milestone, :reject_if => :all_blank, :update_only => true, :allow_destroy => true
+
 end
