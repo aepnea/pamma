d4d60a1b62b65d09c09242561e4f7853555f6cfa
Cristian Vásquez Parga <cvasquez@zaetachile.cl>
2017-01-16 02:43:38 -0300

    algunos pocos fixes

--- app/controllers/postulation_grant_controller.rb
+++ app/controllers/postulation_grant_controller.rb
@@ -1,5 +1,20 @@
 class PostulationGrantController < ApplicationController
   def index
-    @postulations = Postulation.all
+    @br = BenefitRequested.where(postulation_date_id: params[:id])
+  end
+
+  def edit
+  end
+  #### cambiar variable
+  def update
+    respond_to do |format|
+      if @benefit_requested.update(benefit_requested_params)
+        format.html { redirect_to @benefit_requested, notice: 'Benefit requested was successfully updated.' }
+        format.json { render :show, status: :ok, location: @benefit_requested }
+      else
+        format.html { render :edit }
+        format.json { render json: @benefit_requested.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
